529db83a70aec9e9ff171445d7b549c33b660e68
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-06-22 15:23:31 +0530

    spring intial commit with model and controller

---
+++ src/main/java/com/chiran/biguni/socketlovers/socketLovers/SocketLoversApplication.java
@@ -0,0 +1,13 @@
+package com.chiran.biguni.socketlovers.socketLovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketLoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiran/biguni/socketlovers/socketLovers/SocketLoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiran.biguni.socketlovers.socketLovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketLoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
